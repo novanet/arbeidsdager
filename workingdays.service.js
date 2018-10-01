2eafdbb010bf2cdb536cb624c82a12bf1f415d70
Anders Austad <anderaus@gmail.com>
2018-10-01 20:43:55 +0200

    Add editorconfig, fix indentation

--- workingdays.service.js
+++ workingdays.service.js
@@ -1,29 +1,27 @@
 var utils = utils || {};
-utils.wdays = utils.wdays || {};
-
 (function (ctx) {
 
-    ctx.getNumberOfWorkingDays = function (year, month, holidays) {
-        var lastDayOfMonth = moment(year + '-' + month, 'YYYY-MM').daysInMonth();
+  ctx.getNumberOfWorkingDays = function (year, month, holidays) {
+    var lastDayOfMonth = moment(year + '-' + month, 'YYYY-MM').daysInMonth();
 
-        var numberOfWorkingDays = 0;
-        for (var i = 1; i <= lastDayOfMonth; i++) {
-            var date = moment(new Date(year, month - 1, i));
+    var numberOfWorkingDays = 0;
+    for (var i = 1; i <= lastDayOfMonth; i++) {
+      var date = moment(new Date(year, month - 1, i));
 
-            if (!_isWeekend(date) && !_isHoliday(date, holidays)) {
-                numberOfWorkingDays++;
-            }
-        }
-        return numberOfWorkingDays;
+      if (!_isWeekend(date) && !_isHoliday(date, holidays)) {
+        numberOfWorkingDays++;
+      }
     }
+    return numberOfWorkingDays;
+  }
 
-    function _isHoliday(date, holidays) {
-        return _.some(holidays, function (holiday) {
-            return moment(holiday.date).isSame(date, 'day');
-        });
-    }
+  function _isHoliday(date, holidays) {
+    return _.some(holidays, function (holiday) {
+      return moment(holiday.date).isSame(date, 'day');
+    });
+  }
 
-    function _isWeekend(date) {
-        return date.day() === 6 || date.day() === 0;
-    }
-})(utils.wdays)
+  function _isWeekend(date) {
+    return date.day() === 6 || date.day() === 0;
+  }
+})(utils || {});
