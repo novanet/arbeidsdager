5d6aa88c67db100d866e175de1df79a137f7b5c2
Anders Austad <anderaus@gmail.com>
2018-10-01 19:35:24 +0200

    Add days/week numbers

--- app.js
+++ app.js
@@ -4,20 +4,25 @@ new Vue({
 
     data: function () {
         return {
-            selectedYear: 0
+            selectedYear: 0,
+            today: '',
+            week: '',
+            months: [],
+            holidays: [],
+            totalNumberOfWorkingDaysInYear: 0
         }
     },
 
     created: function () {
-        this.selectedYear = moment().year()
+        this.selectedYear = moment().year();
+        this.today = moment().format();
+        this.week = moment().isoWeek();
     },
 
-    computed: {
-        holidays() {
-            return utils.holiday.getHolidays(this.selectedYear)
-        },
-        months() {
-            return [
+    watch: {
+        selectedYear: function (year) {
+            this.holidays = utils.holiday.getHolidays(this.selectedYear);
+            this.months = [
                 { 'name': 'Januar', 'numberOfWorkingDays': utils.wdays.getNumberOfWorkingDays(this.selectedYear, 1, this.holidays) },
                 { 'name': 'Februar', 'numberOfWorkingDays': utils.wdays.getNumberOfWorkingDays(this.selectedYear, 2, this.holidays) },
                 { 'name': 'Mars', 'numberOfWorkingDays': utils.wdays.getNumberOfWorkingDays(this.selectedYear, 3, this.holidays) },
@@ -30,7 +35,12 @@ new Vue({
                 { 'name': 'Oktober', 'numberOfWorkingDays': utils.wdays.getNumberOfWorkingDays(this.selectedYear, 10, this.holidays) },
                 { 'name': 'November', 'numberOfWorkingDays': utils.wdays.getNumberOfWorkingDays(this.selectedYear, 11, this.holidays) },
                 { 'name': 'Desember', 'numberOfWorkingDays': utils.wdays.getNumberOfWorkingDays(this.selectedYear, 12, this.holidays) }
-            ]
+            ];
+            var totalDays = 0;
+            for (var i = 0; i < this.months.length; i++) {
+                totalDays += this.months[i].numberOfWorkingDays;
+            }
+            this.totalNumberOfWorkingDaysInYear = totalDays;
         }
     },
 
@@ -42,4 +52,10 @@ new Vue({
             this.selectedYear++;
         }
     },
+
+    filters: {
+        date: function (date, format) {
+            return moment(date).format(format);
+        }
+    }
 })
